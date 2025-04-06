dc9682e58d96b550eb74ec9ec1e0b59987173b15
Иван Трајков <139054085+ivantrajkov@users.noreply.github.com>
2025-04-06 10:24:48 +0200

    Update List.jsx

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { ApolloClient, InMemoryCache, ApolloProvider, gql, useQuery } from '@apollo/client';
 import '../css/custom-css.css';
-import '../i18n'; // Make sure this file is imported to initialize i18n
+import '../i18n';
 import { useTranslation } from 'react-i18next';
 
 const client = new ApolloClient({
